4171c32ebcfb28d1bfa0642b52a8e1ddaa861ef0
Michel Lieben <liebenmichel@gmail.com>
2017-12-16 16:37:29 +0100

    k

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,15 @@
 
 Cocktail.destroy_all
 
-# cocktails = Cocktail.create([
-#   { name: 'Mojito',
-#     Ingredient.create([
-#       { name: 'Lime Wedges'},
-#       { name: '4pcs'}
-#       ])
-#     },
-#   { name: 'Caipiroska'}
-#  ])
-
 cocktails = Cocktail.create([
   { name: 'Mojito', photo: "photo-1455621481073-d5bc1c40e3cb_spo1sf.jpg", ingredients_attributes: [
     { name: 'Recette1', quantity: '5cl' },
     { name: 'Recette2', quantity: '15cl' },
     { name: 'Recette3', quantity: 'Top' }
+    ] },
+  { name: 'Caipiroska', photo: 'Drink_vvd5ch.jpg', ingredients_attributes: [
+    { name: 'Ingredient_1', quantity: '4 sugars' },
+    { name: 'Ingredient_2', quantity: '3 blabla' }
     ] },
   { name: 'Caipiroska', photo: 'Drink_vvd5ch.jpg', ingredients_attributes: [
     { name: 'Ingredient_1', quantity: '4 sugars' },
